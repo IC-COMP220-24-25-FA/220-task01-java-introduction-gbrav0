9b51e74fbae950e38dee251cf3633d1cbeffcb78
gbrav0 <gbravo@ithaca.edu>
2024-09-01 22:56:24 -0400

    Added methods for triangle. Wrote constructor test. Defined attributes for class Triangle.

--- src/main/java/edu/ithaca/dragon/shapes/Triangle.java
+++ src/main/java/edu/ithaca/dragon/shapes/Triangle.java
@@ -1,5 +1,27 @@
 package edu.ithaca.dragon.shapes;
 
 public class Triangle {
-    
+    private double sideA;
+    private double sideB;
+    private double sideC;
+
+
+    public Triangle(double sideA, double sideB, double sideC){
+        throw new IllegalAccessError("Not implemented yet");
+    }
+
+    public double calcArea(){
+        throw new IllegalAccessError("Not implemented yet");
+
+    }
+
+    public double longestLineWithin(){
+        throw new IllegalAccessError("Not implemented yet");
+
+    }
+
+    public double doubleSize(){
+        throw new IllegalAccessError("Not implemented yet");
+
+    }
 }
--- src/test/java/edu/ithaca/dragon/shapes/TriangleTest.java
+++ src/test/java/edu/ithaca/dragon/shapes/TriangleTest.java
@@ -1,5 +1,17 @@
 package edu.ithaca.dragon.shapes;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
 public class TriangleTest {
+
+    @Test 
+    public void TestConstructorError(){
+        assertThrows(IllegalArgumentException.class, () -> new Triangle(1,2,0));
+    }
     
 }
